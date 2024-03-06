44ebcb03cac98d7e8082caa0757e437d4ce4d804
Kenny Goodin <kennethbgoodin@gmail.com>
2024-03-06 14:42:09 -0500

    add 80 remove duplicates from sorted array 2

---
+++ 80-remove-duplicates-2/remove.go
@@ -0,0 +1,25 @@
+package removeduplicates2
+
+func removeDuplicates(nums []int) int {
+	counts := map[int]int{}
+	j := 0
+	for i := 0; i < len(nums); i++ {
+		a := nums[i]
+		if c, ok := counts[a]; !ok || c+1 <= 2 {
+			nums[j] = nums[i]
+			j++
+			if !ok {
+				counts[a] = 1
+				continue
+			}
+		}
+
+		counts[a] = counts[a] + 1
+	}
+
+	nums = nums[:j]
+	for i := 0; i < len(nums)-j; i++ {
+		nums = append(nums, -1)
+	}
+	return j
+}
---
+++ 80-remove-duplicates-2/remove_test.go
@@ -0,0 +1,39 @@
+package removeduplicates2
+
+import "testing"
+
+func Test_removeDuplicates(t *testing.T) {
+	type args struct {
+		nums []int
+	}
+	tests := []struct {
+		name     string
+		args     args
+		want     int
+		wantNums []int
+	}{
+		{
+			name: "1",
+			args: args{
+				nums: []int{1, 1, 1, 2, 2, 3},
+			},
+			want:     5,
+			wantNums: []int{1, 1, 2, 2, 3, -1},
+		},
+		{
+			name: "2",
+			args: args{
+				nums: []int{0, 0, 1, 1, 1, 1, 2, 3, 3},
+			},
+			want:     7,
+			wantNums: []int{0, 0, 1, 1, 2, 3, 3, -1, -1},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := removeDuplicates(tt.args.nums); got != tt.want {
+				t.Errorf("removeDuplicates() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
