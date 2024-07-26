97b2277a971c18cfd9e87b7cd2a9ac4faa3289b5
Kenny Goodin <kennethbgoodin@gmail.com>
2024-07-26 14:08:21 -0400

    add 55 jump game

--- 55-jump-game/jump.go
+++ 55-jump-game/jump.go
@@ -1,8 +1,22 @@
 package jump
 
 func canJump(nums []int) bool {
-	// 2,3,1,1,4
-	pos := 0
+	if len(nums) < 1 {
+		return true
+	}
 
-	return true
+	gas := 0
+	for i, n := range nums {
+		if gas < 0 {
+			return false
+		} else if n > gas {
+			gas = n
+		}
+		if i+1 >= len(nums) {
+			return true
+		}
+		gas -= 1
+	}
+
+	return false
 }
--- 55-jump-game/jump_test.go
+++ 55-jump-game/jump_test.go
@@ -1,29 +1,32 @@
 package jump
 
-import "testing"
+import (
+	"strconv"
+	"testing"
+)
 
 func Test_canJump(t *testing.T) {
 	type args struct {
 		nums []int
 	}
 	tests := []struct {
-		name string
 		args args
 		want bool
 	}{
 		{
-			name: "1",
+			args: args{nums: []int{1, 1, 1, 0}},
+			want: true,
+		}, {
 			args: args{nums: []int{2, 3, 1, 1, 4}},
 			want: true,
 		},
 		{
-			name: "2",
 			args: args{nums: []int{3, 2, 1, 0, 4}},
 			want: false,
 		},
 	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
+	for i, tt := range tests {
+		t.Run(strconv.Itoa(i), func(t *testing.T) {
 			if got := canJump(tt.args.nums); got != tt.want {
 				t.Errorf("canJump() = %v, want %v", got, tt.want)
 			}
